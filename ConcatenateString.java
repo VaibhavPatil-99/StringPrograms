b433c31cacc21b8a38b3170879305cbe1866f17e
Vaibhav Patil <110226013+VaibhavPatil-99@users.noreply.github.com>
2024-02-08 23:48:36 +0530

    Add files via upload

---
+++ ConcatenateString.java
@@ -0,0 +1,15 @@
+package string;
+
+public class ConcatenateString {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		String s1="hello";
+		String s2="world";
+		
+		s1=s1+ " "+s2;
+		System.out.println(s1);
+	}
+
+}
