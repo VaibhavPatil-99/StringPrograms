74d66b2904dce8466405a477761c3a57c5910d5d
Vaibhav Patil <110226013+VaibhavPatil-99@users.noreply.github.com>
2024-02-08 23:48:17 +0530

    Add files via upload

---
+++ CompareTo.java
@@ -0,0 +1,18 @@
+package string;
+
+// first take second string to check and compare with string 1
+// compare b from second string with a of first string
+
+
+public class CompareTo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		String s1 = "abc";
+		String s2 = "bbc";
+		
+		System.out.println(s1.compareTo(s2));;
+	}
+
+}
