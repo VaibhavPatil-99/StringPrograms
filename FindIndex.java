b9d0b4ffb9941c353ffe64460968603763376beb
Vaibhav Patil <110226013+VaibhavPatil-99@users.noreply.github.com>
2024-02-10 23:38:10 +0530

    Add files via upload

---
+++ FindIndex.java
@@ -0,0 +1,15 @@
+package string;
+
+// Find index value using method 
+
+public class FindIndex {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		String s1 = "welcome";
+		int index = 3;
+		System.out.println(s1.charAt(index));
+	}
+
+}
