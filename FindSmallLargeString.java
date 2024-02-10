d3bcbb698c99e2bc53a86f48f9a1311137ac662a
Vaibhav Patil <110226013+VaibhavPatil-99@users.noreply.github.com>
2024-02-10 23:38:52 +0530

    Add files via upload

---
+++ FindSmallLargeString.java
@@ -0,0 +1,28 @@
+  package string;
+
+public class FindSmallLargeString {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		String s1= "welcome to java programming";
+		String sa[]=s1.split(" ");
+		
+		for(int i=0;i<sa.length;i++) {
+			for(int j=i+1;j<sa.length;j++) {
+				if(sa[i].length()>sa[j].length())
+				{
+					String temp=sa[i];
+					sa[i]=sa[j];
+					sa[j]=temp;
+				}
+			}
+		}
+//		for(int i=0;i<sa.length;i++) {
+//			System.out.println(sa[i]);
+//		}
+		System.out.println("smallest word : "+sa[0]);
+		System.out.println("largest word : "+sa[sa.length-1]);
+	}
+
+}
