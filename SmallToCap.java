baafc3e94f3f47a87f28f38c436c32b4f0dec748
Vaibhav Patil <110226013+VaibhavPatil-99@users.noreply.github.com>
2024-02-14 22:02:57 +0530

    Add files via upload

---
+++ SmallToCap.java
@@ -0,0 +1,24 @@
+package string;
+
+// covert string small to CAPITAL.
+
+public class SmallToCap {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		String s1 = "welcome";
+		String s2="";
+		
+		for(int i=0;i<s1.length();i++) {
+			if(s1.charAt(i)>='a' && s1.charAt(i)<='z') {
+				s2 = s2 +(char)(s1.charAt(i)-32);
+			}
+			else{
+				s2=s2+s1.charAt(i);
+			}
+		}
+		System.out.println(s2);
+	}
+
+}
