74810796b32ac4fa075a8bf777e9b043ef31e13f
Vaibhav Patil <110226013+VaibhavPatil-99@users.noreply.github.com>
2024-02-22 19:37:24 +0530

    Add files via upload

---
+++ RevSubarray.java
@@ -0,0 +1,18 @@
+package string_Variation;
+
+public class RevSubarray {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int a[] = {1,2,3,4,5,6};
+		int b[] = new int[a.length];
+		int k=3;
+	
+		for(int i=0;i<a.length;i++)
+		{
+			
+		}
+	}
+
+}
